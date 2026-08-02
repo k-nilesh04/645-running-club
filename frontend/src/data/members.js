887001b23d667a551406e714570a356ce43022cd
k-nilesh04 <k.nilesh0623@gmail.com>
2026-08-02 15:45:56 +0530

    title changes

--- frontend/src/data/members.js
+++ frontend/src/data/members.js
@@ -4,7 +4,6 @@ const members = [
   { _id: "2", name: "Aakash ", age: 19, runsCompleted: 35, distanceKm: 190, memberSince: 2026 },
   { _id: "3", name: "Matrix", age: 19, runsCompleted: 30, distanceKm: 165, memberSince: 2026 },
   { _id: "4", name: "Ankit", age: 20, runsCompleted: 28, distanceKm: 150, memberSince: 2026 },
-  { _id: "5", name: "Radhika", age: 22, runsCompleted: 25, distanceKm: 132, memberSince: 2026 },
 ];
 
 export default members;
